de37d7c47eec1d7e1e27ea3c25d9e81b1504e30b
Benjamin <scyjw12@nottingham.edu.cn>
2023-09-17 15:17:21 +0800

    程序逻辑控制练习

--- logical_control/src/logical_control.java
+++ logical_control/src/logical_control.java
@@ -106,13 +106,26 @@ public class logical_control {
         }
     }
 
-    public static void main7(String[] args) {
+    public static void main(String[] args) {
         //编写代码模拟三次密码输入的场景。 最多能输入三次密码，密码正确，提示“登录成功”,密码错误，
         // 可以重新输 入，最多输入三次。三次均错，则提示退出程序
-
+        int count = 3;
+        String password = "Wjt164673";
+        while (count > 0) {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            if (input.equals(password)) {
+                System.out.println("密码正确");
+                break;
+            }
+            else {
+                count--;
+                System.out.println("密码错误，还剩"+count+"次");
+            }
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main8(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         for (int i = 1; i <= num; i++) {
