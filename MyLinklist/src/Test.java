21956f0984b52cb5577c9ee8a64ec9543711a44a
Benjamin <scyjw12@nottingham.edu.cn>
2023-10-19 17:01:26 +0800

    不带头双链表不循环

--- MyLinklist/src/Test.java
+++ MyLinklist/src/Test.java
@@ -6,7 +6,7 @@
  * Time: 19:22
  */
 public class Test {
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         MyLinklist ml = new MyLinklist();
         ml.addFirst(21);
         ml.addFirst(21);
@@ -17,5 +17,17 @@ public class Test {
         ml.addIndex(0,23);
         ml.display();
 
+    }
+
+    public static void main(String[] args) {
+        MyLinkedlist ml = new MyLinkedlist();
+        ml.addFirst(21);
+        ml.addLast(54);
+        ml.addIndex(1,67);
+        ml.remove(67);
+        ml.removeAllKey(21);
+        ml.addIndex(0,23);
+        ml.display();
+        ml.clear();
     }
 }
