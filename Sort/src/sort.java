5808f9fb2282bfe07ccee2fa574a413a7c4d627b
Benjamin <scyjw12@nottingham.edu.cn>
2023-11-19 21:03:57 +0800

    排序：快排与冒泡

--- Sort/src/sort.java
+++ Sort/src/sort.java
@@ -49,44 +49,45 @@ public class sort {
     }
 
     // 选择排序
-    public static void selectSort(int[]array) {
+    public static void selectSort(int[] array) {
         for (int i = 0; i < array.length; i++) {
             int minindex = i;
-            for (int j = i+1; j <array.length; j++) {
-                if(array[j]<array[minindex]){
-                    minindex=j;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minindex]) {
+                    minindex = j;
                 }
             }
-            swap(array,i,minindex);
+            swap(array, i, minindex);
         }
 
     }
 
-    private static void swap(int[] arr,int a, int b) {
+    private static void swap(int[] arr, int a, int b) {
         int temp = arr[a];
-        arr[a]= arr[b];
-        arr[b]=temp;
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 
 
     // 堆排序
     public static void heapSort(int[] array) {
         createheap(array);
-        for (int i = array.length-1; i >0; i--) {
-            swap(array,i,0);
-            shiftDown(0,i,array);
+        for (int i = array.length - 1; i > 0; i--) {
+            swap(array, i, 0);
+            shiftDown(0, i, array);
         }
     }
 
     private static void createheap(int[] array) {
-        int len = array.length-1;
-        int parent = (len-1-1)/2;
-        while(parent>=0){
-            shiftDown(parent,len,array);
+        int len = array.length - 1;
+        int parent = (len - 1 - 1) / 2;
+        while (parent >= 0) {
+            shiftDown(parent, len, array);
             parent--;
         }
     }
-    private static void shiftDown(int parent,int usedsize,int[] heap) {
+
+    private static void shiftDown(int parent, int usedsize, int[] heap) {
         //左节点
         int child = parent * 2 + 1;
         while (child < usedsize) {
@@ -96,10 +97,58 @@ public class sort {
             }
             //找到子节点中的最大值
             if (heap[child] > heap[parent]) {
-                swap(heap,child, parent);
+                swap(heap, child, parent);
             }
             parent = child;
             child = parent * 2 + 1;
         }
+    }
+
+    // 冒泡排序
+    public static void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int flag = 0;
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    swap(array, j, j + 1);
+                    flag = 1;
+                }
+            }
+            if (flag == 0) {
+                break;
+            }
+        }
+    }
+
+    // 快速
+    public static void quickSort(int[] array) {
+        helpQuick(array, 0, array.length - 1);
+    }
+
+    private static void helpQuick(int[] array, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int div = partion(array, left, right);
+        helpQuick(array, left, div - 1);
+        helpQuick(array, div + 1, right);
+    }
+
+    //挖坑
+    private static int partion(int[] array, int left, int right) {
+        int temp = array[left];
+        while (left < right) {
+
+            while (left < right && array[right] >= temp) {
+                right--;
+            }
+            array[left] = array[right];
+            while (left < right && array[left] <= temp) {
+                left++;
+            }
+            array[right] = array[left];
+        }
+        array[left]= temp;
+        return left;
     }
 }
--- Sort/src/test.java
+++ Sort/src/test.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 public class test {
     public static void main(String[] args) {
         int []arr ={3,4,63,2,53,39,23,56,2};
-        sort.heapSort(arr);
+        sort.quickSort(arr);
         System.out.println(Arrays.toString(arr));
 
     }
