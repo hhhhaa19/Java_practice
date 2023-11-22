0b2407fd9136a0835f11d17977badd3bb25a2206
Benjamin <scyjw12@nottingham.edu.cn>
2023-11-22 18:09:13 +0800

    排序完成

--- Sort/src/sort.java
+++ Sort/src/sort.java
@@ -1,3 +1,6 @@
+import java.util.Stack;
+
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -62,6 +65,27 @@ public class sort {
 
     }
 
+    //选排排左右同时进行
+    public static void selectSort2(int[] array) {
+        for (int i = 0; i < array.length - i; i++) {
+            int minindex = i;
+            int maxindex = array.length - i - 1;
+            for (int j = i; j < array.length - i; j++) {
+                if (array[j] < array[minindex]) {
+                    minindex = j;
+                }
+                if (array[j] > array[maxindex]) {
+                    maxindex = j;
+                }
+            }
+            swap(array, i, minindex);
+            if (maxindex == i) {
+                maxindex = minindex;
+            }
+            swap(array, array.length - i - 1, maxindex);
+        }
+    }
+
     private static void swap(int[] arr, int a, int b) {
         int temp = arr[a];
         arr[a] = arr[b];
@@ -72,14 +96,15 @@ public class sort {
     // 堆排序
     public static void heapSort(int[] array) {
         createheap(array);
-        for (int i = array.length - 1; i > 0; i--) {
+        for (int i = array.length - 1; i >= 0; i--) {
             swap(array, i, 0);
+            //堆排与建堆不同，最后一个数不能算进去
             shiftDown(0, i, array);
         }
     }
 
     private static void createheap(int[] array) {
-        int len = array.length - 1;
+        int len = array.length;
         int parent = (len - 1 - 1) / 2;
         while (parent >= 0) {
             shiftDown(parent, len, array);
@@ -121,17 +146,59 @@ public class sort {
     }
 
     // 快速
+    //优化的两种方式:三数中值取k
+    //小区间插排
     public static void quickSort(int[] array) {
         helpQuick(array, 0, array.length - 1);
     }
 
     private static void helpQuick(int[] array, int left, int right) {
-        if (left >= right) {
+        //最后小区间进行插排
+        if (right - left <= 10) {
+            insertQuickSort(array, left, right);
             return;
         }
+        int mid = midArray(array, left, right);
+        swap(array, mid, left);
         int div = partion(array, left, right);
         helpQuick(array, left, div - 1);
         helpQuick(array, div + 1, right);
+    }
+
+    private static void insertQuickSort(int[] array, int left, int right) {
+        for (int i = left; i <= right; i++) {
+            int minindex = i;
+            for (int j = i + 1; j <= right; j++) {
+                if (array[j] < array[minindex]) {
+                    minindex = j;
+                }
+            }
+            swap(array, i, minindex);
+        }
+    }
+
+
+    //找到array[left],array[right],中位数三数的中间大小的值，返回下标
+    private static int midArray(int[] array, int left, int right) {
+        int mid = (left + right) / 2;
+        if (array[left] < array[right]) {
+            if (array[mid] < array[left]) {
+                return left;
+            } else if (array[mid] > array[right]) {
+                return right;
+            } else {
+                return mid;
+            }
+        } else {
+            //array[left] > array[right]
+            if (array[mid] < array[right]) {
+                return right;
+            } else if (array[mid] > array[left]) {
+                return left;
+            } else {
+                return mid;
+            }
+        }
     }
 
     //挖坑
@@ -148,7 +215,156 @@ public class sort {
             }
             array[right] = array[left];
         }
-        array[left]= temp;
+        array[left] = temp;
         return left;
+    }
+
+    //hoare版
+    private static int partion2(int[] array, int left, int right) {
+        int temp = array[left];
+        int temIndex = left;
+        while (left < right) {
+
+            while (left < right && array[right] >= temp) {
+                right--;
+            }
+            while (left < right && array[left] <= temp) {
+                left++;
+            }
+            swap(array, left, right);
+        }
+        swap(array, temIndex, right);
+        return left;
+    }
+    //前后指针，前指针存，后指针找
+    private static int partition3(int[] array, int left, int right) {
+        int prev = left ;
+        int cur = left+1;
+        while (cur <= right) {
+            if(array[cur] < array[left] && array[++prev] != array[cur]) {
+                swap(array,cur,prev);
+            }
+            cur++;
+        }
+
+        swap(array,prev,left);
+        return prev;
+    }
+    //快排，非递归
+    public static void quickSortNor(int[] array) {
+        Stack<Integer> st = new Stack<>();
+        int div = partion(array, 0, array.length - 1);
+        if (div - 1 >= 2) {
+            st.push(0);
+            st.push(div - 1);
+        }
+        if (array.length - (div + 1) >= 2) {
+            st.push(div + 1);
+            st.push(array.length - 1);
+        }
+        while (!st.isEmpty()) {
+            int right = st.pop();
+            int left = st.pop();
+            div = partion(array, left, right);
+            if (div - 1 - left >= 2) {
+                st.push(0);
+                st.push(div - 1);
+            }
+            if (right - (div + 1) >= 2) {
+                st.push(div + 1);
+                st.push(right);
+            }
+        }
+    }
+
+    public static void mergeSort(int[] array) {
+        helpMergeSort(array,0, array.length-1);
+    }
+
+    private static void helpMergeSort(int[] array, int left, int right) {
+        if(left>=right){
+            return;
+        }
+        int mid = (left+right)/2;
+        helpMergeSort(array,left,mid);
+        helpMergeSort(array,mid+1,right);
+        merge(array,left,mid,right);
+    }
+    private static void merge(int[] array,int left,int mid, int right ){
+        int s1 = left;//可以不定义，这样写为了好理解
+        int e1 = mid;//可以不定义，这样写为了好理解
+        int s2 = mid+1;
+        int e2 = right;//可以不定义，这样写为了好理解
+        //定义一个新的数组
+        int[] tmpArr = new int[right-left+1];
+        int k = 0;//tmpArr数组的下标
+        //同时满足 证明两个归并段 都有数据
+        while (s1 <= e1 && s2 <= e2) {
+            if(array[s1] <= array[s2]) {
+                tmpArr[k++] = array[s1++];
+            }else {
+                tmpArr[k++] = array[s2++];
+            }
+        }
+        while (s1 <= e1) {
+            tmpArr[k++] = array[s1++];
+        }
+        while (s2 <= e2) {
+            tmpArr[k++] = array[s2++];
+        }
+        //把排好序的数据 拷贝回原来的数组array当中
+        for (int i = 0; i < tmpArr.length; i++) {
+            array[i+left] = tmpArr[i];
+        }
+    }
+    public static void mergeSortNor(int[] array) {
+        int gap = 1;//每组几个数据
+        while(gap< array.length){
+            for (int i = 0; i < array.length; i+=gap*2) {
+                int left = i;
+                int right = i+(gap*2)-1;
+                int mid =mid=(left+right)/2;
+                if(right>= array.length-1){
+                    right = array.length-1;
+                }
+                if(mid>= array.length-1){
+                    mid = array.length-1;
+                }
+                merge(array,left,mid,right);
+            }
+            gap *= 2;
+        }
+    }
+    //计数排序
+    public static void countSort(int[] array) {
+        //求数组的最大值 和 最小值  O(N)
+        int minVal = array[0];
+        int maxVal = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if(array[i] < minVal) {
+                minVal = array[i];
+            }
+            if(array[i] > maxVal) {
+                maxVal = array[i];
+            }
+        }
+        //确定计数数组的 长度
+        int len = maxVal - minVal + 1;
+        int[] count = new int[len];
+        //遍历array数组 把数据出现的次数存储到计数数组当中   O(N)
+        for (int i = 0; i < array.length; i++) {
+            count[array[i]-minVal]++;
+        }
+        //计数数组已经存放了每个数据出现的次数
+        //遍历计数数组 把实际的数据写回array数组  O(范围)
+        int index = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                //这里需要重写写回array 意味着得从array的0位置开始写
+                array[index] = i+minVal;
+                index++;
+                count[i]--;
+            }
+        }
     }
 }
--- Sort/src/test.java
+++ Sort/src/test.java
@@ -1,5 +1,5 @@
 import java.util.Arrays;
-
+import java.util.Random;
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -8,10 +8,115 @@ import java.util.Arrays;
  * Time: 19:03
  */
 public class test {
-    public static void main(String[] args) {
-        int []arr ={3,4,63,2,53,39,23,56,2};
-        sort.quickSort(arr);
-        System.out.println(Arrays.toString(arr));
+
+
+
+    private static void testQuick(int[] array) {
+        int[] temArray = Arrays.copyOf(array, array.length);
+        long startTime = System.currentTimeMillis();
+        sort.quickSort(temArray);
+        long endTime = System.currentTimeMillis();
+        System.out.println("快排优化"+(endTime-startTime));
+    }
+
+    private static void testBubble(int[] array) {
+        int[] temArray = Arrays.copyOf(array, array.length);
+        long startTime = System.currentTimeMillis();
+        sort.bubbleSort(temArray);
+        long endTime = System.currentTimeMillis();
+        System.out.println("冒泡优化"+(endTime-startTime));
+    }
+
+    private static void testHeap(int[] array) {
+        int[] temArray = Arrays.copyOf(array, array.length);
+        long startTime = System.currentTimeMillis();
+        sort.heapSort(temArray);
+        long endTime = System.currentTimeMillis();
+        System.out.println("堆排"+(endTime-startTime));
 
     }
+
+    private static void testSelect(int[] array) {
+        int[] temArray = Arrays.copyOf(array, array.length);
+        long startTime = System.currentTimeMillis();
+        sort.selectSort(temArray);
+        long endTime = System.currentTimeMillis();
+        System.out.println("选排优化"+(endTime-startTime));
+
+    }
+
+    private static void testShell(int[] array) {
+        int[] temArray = Arrays.copyOf(array, array.length);
+        long startTime = System.currentTimeMillis();
+        sort.shellSort(temArray);
+        long endTime = System.currentTimeMillis();
+        System.out.println("希尔优化"+(endTime-startTime));
+    }
+
+    private static void testInsert(int[] array) {
+        int[] temArray = Arrays.copyOf(array, array.length);
+        long startTime = System.currentTimeMillis();
+        sort.insertSort(temArray);
+        long endTime = System.currentTimeMillis();
+        System.out.println("插排优化"+(endTime-startTime));
+
+    }
+
+
+    private static void randOrder(int[] array) {
+        Random random= new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i]=random.nextInt(10000);
+        }
+    }
+
+    private static void initOrder(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i]=i;
+        }
+    }
+    private static void testQuickNor(int[] array) {
+        int[] temArray = Arrays.copyOf(array, array.length);
+        long startTime = System.currentTimeMillis();
+        sort.quickSortNor(temArray);
+        long endTime = System.currentTimeMillis();
+        System.out.println("快排非递归"+(endTime-startTime));
+    }
+
+    private static void testMergeNor(int[] array) {
+        int[] temArray = Arrays.copyOf(array, array.length);
+        long startTime = System.currentTimeMillis();
+        sort.mergeSortNor(temArray);
+        long endTime = System.currentTimeMillis();
+        System.out.println("归并排非递归"+(endTime-startTime));
+    }
+
+    private static void testMerge(int[] array) {
+        int[] temArray = Arrays.copyOf(array, array.length);
+        long startTime = System.currentTimeMillis();
+        sort.mergeSort(temArray);
+        long endTime = System.currentTimeMillis();
+        System.out.println("归并排"+(endTime-startTime));
+    }
+    public static void main1(String[] args) {
+        int []arr ={3,4,63,2,53,39,23,56,2};
+        sort.quickSortNor(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void main(String[] args) {
+        int[] array = new int[10000];
+        //initOrder(array);
+        randOrder(array);
+        testInsert(array);
+        testShell(array);
+        testSelect(array);
+        testHeap(array);
+        testBubble(array);
+        testQuick(array);
+        testMerge(array);
+        testMergeNor(array);
+        testQuickNor(array);
+    }
+
+
 }
